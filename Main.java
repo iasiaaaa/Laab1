7e5904caead27ad369e8ea985919d419ab4dcff9
Yaroslava Dudnyk <--get>
2025-12-31 15:54:28 +0100

    Add two parallel threads (sum and factorial) in Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,33 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Java проєкт");
+
+        Runnable sumTask = () -> {
+            long sum = 0L;
+            for (int i = 1; i <= 5_000_000; i++) {
+                sum += i;
+            }
+            System.out.println(Thread.currentThread().getName() + " result: sum= " + sum);
+        };
+
+        Runnable factorialTask = () -> {
+            java.math.BigInteger f = java.math.BigInteger.ONE;
+            int n = 2000; // compute factorial(2000)
+            for (int i = 1; i <= n; i++) {
+                f = f.multiply(java.math.BigInteger.valueOf(i));
+            }
+            System.out.println(Thread.currentThread().getName() + " result: factorial(" + n + ") digits=" + f.toString().length());
+        };
+
+        Thread t1 = new Thread(sumTask, "SumThread");
+        Thread t2 = new Thread(factorialTask, "FactThread");
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Both threads finished.");
     }
 }
